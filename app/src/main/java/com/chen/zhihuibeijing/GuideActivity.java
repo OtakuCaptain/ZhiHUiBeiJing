78727a4b11c742a2914b95687c26df6ac61c8ba0
OtakuCaptain <chen_t_y@hotmail.com>
2016-12-16 21:43:55 +0800

    导航界面完成

--- app/src/main/java/com/chen/zhihuibeijing/GuideActivity.java
+++ app/src/main/java/com/chen/zhihuibeijing/GuideActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -17,27 +20,76 @@ public class GuideActivity extends AppCompatActivity {
     private ViewPager mViewPager;
 
     private ArrayList<ImageView> mImageViewList;
-    private int[] mImageIds=new int[]{R.drawable.guide_1,R.drawable.guide_2,R.drawable.guide_3};
+    private int[] mImageIds = new int[]{R.drawable.guide_1, R.drawable.guide_2, R.drawable.guide_3};
+    private LinearLayout llContainer;
+    private ImageView ivRedPoint;
+    private int mPointDis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_guide);
         mViewPager = (ViewPager) findViewById(R.id.vp_guide);
+        llContainer = (LinearLayout) findViewById(R.id.ll_container);
+        ivRedPoint = (ImageView) findViewById(R.id.iv_red_point);
         initData();
         mViewPager.setAdapter(new GuideAdapter());
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                //滑动过程中回调
+                int leftMargin = (int) (mPointDis * positionOffset)+position*mPointDis;//计算小红点当前的左边距
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) ivRedPoint.getLayoutParams();
+                layoutParams.leftMargin = leftMargin;//修改左边距
+                ivRedPoint.setLayoutParams(layoutParams);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        ivRedPoint.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                ivRedPoint.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                //layout方法执行结束后的回调
+                mPointDis = llContainer.getChildAt(1).getLeft() - llContainer.getChildAt(0).getLeft();
+            }
+        });
+
     }
 
-    private void initData(){
+    private void initData() {
 
-        mImageViewList=new ArrayList<>();
+        mImageViewList = new ArrayList<>();
         for (int i = 0; i < mImageIds.length; i++) {
-            ImageView view=new ImageView(this);
+            ImageView view = new ImageView(this);
             view.setBackgroundResource(mImageIds[i]);
             mImageViewList.add(view);
+
+            //初始化圆点
+            ImageView point = new ImageView(this);
+            point.setImageResource(R.drawable.shape_point_gray);//设置图片
+            //初始化布局参数
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            if (i > 0) {
+                //设置左边距
+                params.leftMargin = 20;
+            }
+            point.setLayoutParams(params);
+            llContainer.addView(point);
         }
     }
+
 
     class GuideAdapter extends PagerAdapter {
 
@@ -54,7 +106,6 @@ public class GuideActivity extends AppCompatActivity {
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-
             ImageView view = mImageViewList.get(position);
             container.addView(view);
             return view;
@@ -62,7 +113,7 @@ public class GuideActivity extends AppCompatActivity {
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View)object);
+            container.removeView((View) object);
         }
     }
 }
